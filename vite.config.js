e38e273f9c8471d52cfe696dae5058c07c18cbf1
kris <lavallakris@gmail.com>
2025-11-23 18:56:21 -0500

    commit

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // Set base path for GitHub Pages deployment (replace with your repo name if different)
-  base: '/tti-dashboard/',
-})
